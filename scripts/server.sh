9180eda7e88c8b6c97303d9429978bc90c26e554
Gonçalo Melo <gtmelo@gmail.com>
2022-06-10 19:00:09 +0100

    Removed unnecessary bash line

--- scripts/server.sh
+++ scripts/server.sh
@@ -8,4 +8,3 @@ steamcmd +force_install_dir "/apps/vrising" +login anonymous +app_update 1829350
 
 Xvfb :0 -screen 0 1024x768x16 &
 DISPLAY=:0.0 wine /apps/vrising/VRisingServer.exe -persistentDataPath "Z:/data"
-/bin/bash
